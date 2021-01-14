6e645be0191cb30a2573dd50940dfb7a8de52a38
Jakub Baierl <jakub.baierl@gmail.com>
2021-01-14 09:22:52 +0100

    Update Container.styles.js

--- src/modules/homepage/Container/Container.styles.js
+++ src/modules/homepage/Container/Container.styles.js
@@ -3,6 +3,10 @@ export const main = () => ({})
 export const title = ({ theme: { colors } }) => ({
     color: colors.fuchsia,
     
+    '& .ant-slider': {
+        color: 'blue',
+    },
+    
     tabletLarge: {
         color: colors.red,
     }
